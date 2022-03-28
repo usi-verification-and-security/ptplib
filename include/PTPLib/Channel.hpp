30639d3d4c6be59e91fe114ec668fa69c623ded2
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-03-28 12:16:41 +0200

    Channel: refactor channel to be single state

--- include/PTPLib/Channel.hpp
+++ include/PTPLib/Channel.hpp
@@ -8,15 +8,17 @@
 #ifndef PTPLIB_CHANNEL_HPP
 #define PTPLIB_CHANNEL_HPP
 
+#include <PTPLib/Header.hpp>
+
 #include <vector>
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#include <deque>
 #include <map>
 #include <algorithm>
 #include <chrono>
-#include "Header.hpp"
+#include <queue>
+
 
 class Channel {
 
@@ -24,57 +26,112 @@ class Channel {
     mutable std::mutex mutex;
     mutable std::condition_variable cv;
 
-    std::deque<std::pair<PTPLib::net::Header, std::string>> queries;
-    std::atomic_bool requestStop;
-    std::atomic_bool stop;
+    typedef std::queue<std::pair<PTPLib::net::Header, std::string>> q_pair_header_str;
+    q_pair_header_str queries;
+    PTPLib::net::Header current_header;
 
+    typedef std::map<std::string, std::vector<std::pair<std::string, int>>> m_str_vec_t;
+    m_str_vec_t m_learned_clauses;
+    m_str_vec_t m_pulled_clauses;
+
+    std::atomic_bool requestStop;
+    std::atomic_bool reset;
+    bool isStopping;
     bool clauseShareMode;
     bool isFirstTime;
-    bool injectClause;
     int clauseLearnDuration;
-    std::string currentSolverAddress;
     bool apiMode;
 
-    std::map<std::string, std::vector<std::pair<std::string, int>>> clauses;
-
 public:
-    Channel() : requestStop(false), stop(false), clauseShareMode(false), isFirstTime(false), injectClause(false),
-                clauseLearnDuration(4000), apiMode(false)
+    Channel()
+    :
+        requestStop         (false),
+        isStopping          (false),
+        reset               (false),
+        clauseShareMode     (false),
+        isFirstTime         (false),
+        clauseLearnDuration (4000),
+        apiMode             (false)
     {}
-
-    void assign(std::vector<std::pair<std::string, int>> & toPublishTerms)
-    {
-        clauses[currentSolverAddress].insert(std::end(clauses[currentSolverAddress]),
-                                             std::begin(toPublishTerms), std::end(toPublishTerms));
-    }
-
-    std::map<std::string, std::vector<std::pair<std::string, int>>> getClauses() { return std::move(clauses); };
 
     std::mutex & getMutex() { return mutex; }
 
-    size_t size() const { return clauses.size(); }
+    void insert_learned_clause(std::vector<std::pair<std::string, int>> & toPublishTerms) {
+        m_learned_clauses[get_current_header().at(PTPLib::Param.NODE)].insert
+        (
+                std::end(m_learned_clauses[get_current_header().at(PTPLib::Param.NODE)]),
+                std::begin(toPublishTerms), std::end(toPublishTerms)
+        );
+    }
 
-    auto cbegin() const { return clauses.cbegin(); }
+    void insert_pulled_clause(std::vector<std::pair<std::string, int>> & toPublishTerms) {
+        m_pulled_clauses[get_current_header().at(PTPLib::Param.NODE)].insert
+        (
+                std::end(m_pulled_clauses[get_current_header().at(PTPLib::Param.NODE)]),
+                std::begin(toPublishTerms), std::end(toPublishTerms)
+        );
+    }
 
-    auto cend() const { return clauses.cend(); }
+    m_str_vec_t extract_learned_clauses() {
+        m_str_vec_t tmp_m(std::move(m_learned_clauses));
+        m_learned_clauses.clear();
+        return tmp_m;
+    };
 
-    auto begin() const { return clauses.begin(); }
+    m_str_vec_t extract_pulled_clauses() {
+        m_str_vec_t tmp_m(std::move(m_pulled_clauses));
+        m_pulled_clauses.clear();
+        return tmp_m;
+    };
 
-    auto end() const { return clauses.end(); }
+    void clear_queries() {
+        q_pair_header_str empty_q;
+        std::swap( get_queris(), empty_q );
+    }
+
+    size_t size_query() const { return queries.size(); }
+
+    bool isEmpty_query() const { return queries.empty(); }
+
+    q_pair_header_str & get_queris() { return queries; }
+
+    std::pair<PTPLib::net::Header, std::string> pop_front_query() {
+        std::pair<PTPLib::net::Header, std::string> tmp_q(std::move(queries.front()));
+        queries.pop();
+        return tmp_q;
+    }
+
+    void push_back_query(std::pair<PTPLib::net::Header, std::string> && hd) { queries.push(std::move(hd)); }
+
+    void set_current_header(PTPLib::net::Header hd) { current_header = hd.copy(hd.keys()); }
+
+    PTPLib::net::Header & get_current_header()  { return current_header; }
+
+    size_t size() const { return m_learned_clauses.size(); }
+
+    auto cbegin() const { return m_learned_clauses.cbegin(); }
+
+    auto cend() const { return m_learned_clauses.cend(); }
+
+    auto begin() const { return m_learned_clauses.begin(); }
+
+    auto end() const { return m_learned_clauses.end(); }
 
     void notify_one() { cv.notify_one(); }
 
     void notify_all() { cv.notify_all(); }
 
-    void clear() { clauses.clear(); }
+    void clear_learned_clauses() { m_learned_clauses.clear(); }
 
-    bool empty() const { return clauses.empty(); }
+    void clear_pulled_clauses() { m_pulled_clauses.clear(); }
 
-    bool shouldTerminate() const { return stop; }
+    bool empty() const { return m_learned_clauses.empty(); }
 
-    void setTerminate() { stop = true; }
+    bool shouldReset() const { return reset; }
 
-    void clearTerminate() { stop = false; }
+    void setReset() { reset = true; }
+
+    void clearReset() { reset = false; }
 
     bool shouldStop() const { return requestStop; }
 
@@ -82,31 +139,17 @@ public:
 
     void clearShouldStop() { requestStop = false; }
 
+    bool shallStop() const { return isStopping; }
+
+    void setShallStop() { isStopping = true; }
+
+    void clearShallStop() { isStopping = false; }
+
     bool isClauseShareMode() const { return clauseShareMode; }
 
     void setClauseShareMode() { clauseShareMode = isFirstTime = true; }
 
     void clearClauseShareMode() { clauseShareMode = false; }
-
-    void pop_front_query() { queries.pop_front(); }
-
-    size_t size_query() const { return queries.size(); }
-
-    bool isEmpty_query() const { return queries.empty(); }
-
-    void clear_query() { queries.clear(); }
-
-    auto cbegin_query() const { return queries.cbegin(); }
-
-    auto cend_query() const { return queries.cend(); }
-
-    auto get_queris() const { return queries; }
-
-    bool shouldInjectClause() const { return injectClause; }
-
-    void setInjectClause() { injectClause = true; }
-
-    void clearInjectClause() { injectClause = false; }
 
     bool getFirstTimeLearnClause() const { return isFirstTime; }
 
@@ -116,38 +159,33 @@ public:
 
     int getClauseLearnDuration() const { return clauseLearnDuration; }
 
-    void push_back_query(std::pair<PTPLib::net::Header, std::string> & header) { queries.push_back(std::move(header)); }
-
     bool isApiMode() const { return apiMode; }
 
     void setApiMode() { apiMode = true; }
 
     void clearApiMode() { apiMode = false; }
 
-    void setCurrentSolverAddress(std::string csa) { currentSolverAddress = csa; }
-
-    std::string getCurrentSolverAddress() const { return currentSolverAddress; }
-
-    bool waitFor(std::unique_lock<std::mutex> & lock, const td_t & timeout_duration)
-    {
-        return cv.wait_for(lock, timeout_duration, [&] { return shouldTerminate(); });
+    bool wait_for(std::unique_lock<std::mutex> & lock, const td_t & timeout_duration) {
+        return cv.wait_for(lock, timeout_duration, [&] { return shouldReset(); });
     }
 
-    void waitQueryOrTermination(std::unique_lock<std::mutex> & lock)
-    {
-        cv.wait(lock, [&] { return (shouldTerminate() or not isEmpty_query()); });
+    void wait_for_queue_or_solver(std::unique_lock<std::mutex> & lock, const td_t & timeout_duration) {
+        cv.wait_for(lock, timeout_duration, [&] { return (shouldReset() or not isEmpty_query() or shallStop()); });
+    }
+
+    void wait(std::unique_lock<std::mutex> & lock) {
+        cv.wait(lock, [&] { return (shouldReset() or not isEmpty_query() or shallStop()); });
     }
 
     void resetChannel() {
-        clear();
-        clear_query();
+        clear_pulled_clauses();
+        clear_learned_clauses();
         clearShouldStop();
-        clearTerminate();
+        clearReset();
         clearClauseShareMode();
-        clearInjectClause();
         setApiMode();
     }
 
 };
 
-#endif PTPLIB_CHANNEL_HPP
+#endif // PTPLIB_CHANNEL_HPP
