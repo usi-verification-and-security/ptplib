d23b76c0676a279127835d7fd62c3a33a640f06b
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-07 00:31:53 +0200

    [ci skip] SMTSEvent: add more constructors

--- include/PTPLib/net/SMTSEvent.hpp
+++ include/PTPLib/net/SMTSEvent.hpp
@@ -19,10 +19,25 @@ namespace PTPLib::net {
     struct SMTS_Event {
         PTPLib::net::Header header;
         std::string body;
+
+        SMTS_Event() {}
+
         SMTS_Event(PTPLib::net::Header && hd, std::string && str) {
             this->header = std::move(hd);
             this->body = std::move(str);
         }
+
+        SMTS_Event(PTPLib::net::Header & hd) {
+            this->header = hd;
+            this->body = std::string();
+        }
+
+        SMTS_Event(PTPLib::net::Header && hd) {
+            this->header = std::move(hd);
+            this->body = std::string();
+        }
+
+        bool empty() const { return header.empty(); }
     };
 }
 
